26184f5cd5dc7fdf9160becd10d63bf2da90c435
HaiyangTian <HaiyangTain@163.com>
2017-09-13 14:39:50 +0800

    添加下面的按钮

--- app/src/main/java/com/example/sun/cashbook/MainActivity.java
+++ app/src/main/java/com/example/sun/cashbook/MainActivity.java
@@ -1,20 +1,27 @@
 package com.example.sun.cashbook;
 
+import android.content.DialogInterface;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.preference.DialogPreference;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.ListViewCompat;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,8 +41,21 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                View viewDialog =LayoutInflater.from(MainActivity.this).inflate(R.layout.cost_et,null);
+                EditText title = viewDialog.findViewById(R.id.et_cost_title);
+                EditText money = viewDialog.findViewById(R.id.et_cost_money);
+                DatePicker date = viewDialog.findViewById(R.id.et_cost_date);
+                builder.setView(viewDialog);
+                builder.setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+
+                });
+                builder.setPositiveButton("cancel",null);
+                builder.create().show();
             }
         });
 
